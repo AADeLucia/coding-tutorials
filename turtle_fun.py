c0423630bc7717fec4a28c87759ce68019d0d5f1
AADeLucia <alexir563@aol.com>
2017-06-26 23:00:19 -0600

    added turtle file

---
+++ turtle_fun.py
@@ -0,0 +1,18 @@
+# Beginning of a python file begins with imports
+import turtle
+
+
+window = turtle.Screen()
+
+# Create a turtle instance and give it a shape
+tammy = turtle.Turtle()
+tammy.shape('turtle')
+
+# Move turtle
+tammy.forward(10)
+tammy.left(10)
+tammy.forward(10)
+
+# Your turn, make the turtle draw a rectangle
+
+turtle.done()
